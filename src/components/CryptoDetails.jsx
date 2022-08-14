c6974b1a25be8c8f9ebe1ffdf643557b9ab9d9c9
batuhanOnur <49373188+batuhanOnur@users.noreply.github.com>
2022-08-14 16:16:56 +0300

    initial setup and routing

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+    return (
+        <div>Homepage</div>
+    )
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+    return (
+        <div>Homepage</div>
+    )
+}
+
+export default Cryptocurrencies
