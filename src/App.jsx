adb08d1a841c62b19c408cec6111e4b76c719bda
batuhanOnur <batuhanonur29@gmail.com>
2022-08-15 17:23:42 +0300

    footer and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,23 @@ const App = () => {
                         </Routes>
                     </div>
                 </Layout>
+<<<<<<< HEAD
             </div>
             <div className="footer">
 
+=======
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br/>
+                        All Rights Reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+>>>>>>> 99717bc (footer and homepage)
             </div>
         </div>
     )
